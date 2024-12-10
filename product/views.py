c4c659ac11450cb3f2d32a41a848ae93203ef7bf
Rennan Silva Dias <28310005@sempreung.com.br>
2024-12-09 22:44:16 -0300

    09_12_24

---
+++ product/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm
+from django.views.generic import ListView,DetailView,DeleteView,CreateView,UpdateView
+from django.urls import reverse_lazy
+from .models import Produto
+
+class ProdutoListView(ListView):
+	model=Produto
+	template_name='produto_list.html'
+
+class ProdutoDetailView(DetailView):
+	model=Produto
+	template_name='produto_detail.html'
+
+class ProdutoCreateView(CreateView):
+	model=Produto
+	template_name='produto_form.html'
+	fields = ['nome','descricao','preco']
+	sucess_url = reverse_lazy('produto_list.html')
+
+class ProdutoUpdateView(UpdateView):
+	model=Produto
+	template_name='produto_form.html'
+	fields = ['nome','descricao','preco']
+	sucess_url = reverse_lazy('produto-list')
+
+class ProdutoDeleteView(DeleteView):
+	model=Produto
+	template_name='produto_confirm_delete.html'
+	fields = ['nome','descricao','preco']
+	sucess_url = reverse_lazy('produto-list')
